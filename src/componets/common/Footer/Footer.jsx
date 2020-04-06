b8cdabcf36030341b618338cd2be7b8f9a2d5333
Pi_phagor <balakin.developer.numo@gmail.com>
2020-04-07 02:17:15 +0300

    fix: size and variables

--- src/componets/common/Footer/Footer.jsx
+++ src/componets/common/Footer/Footer.jsx
@@ -1,58 +1,60 @@
 // core
 import React from 'react';
 
-// styles
-import styles from './Footer.module.scss';
+// components
+import { Link } from "react-router-dom";
 
+// images
 import leedLogo from '../../../assets/images/leed-logo.svg';
 import facebookIcon from '../../../assets/images/facebook-icon.svg';
 import instagramIcon from '../../../assets/images/instagram-icon.svg';
-import { Link } from "react-router-dom";
+
+// styles
+import styles from './Footer.module.scss';
+
+const list = ['Nosotros', 'Vertical', 'Horizontal', 'Comercial', 'Oficinas', 'Financiamiento', 'Relación con Inversionistas'];
 
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <section className={styles.lists}>
-                <div className={styles.logo}>
-                    <Link to='/'><img src={leedLogo} alt='logo' /></Link>
-                    <h3>Construllendo responsablemente</h3>
-                    <p>
-                        Proyectos con Certificacion LEED por
-                        eficiencia energética, iluminación natural
-                        y ventilación diseñada.
-                    </p>
+            <div className={styles.container}>
+            <div className={styles.lists}>
+                    <div className={styles.logo}>
+                        <Link to='/'><img src={leedLogo} alt='logo' /></Link>
+                        <h3>Construllendo responsablemente</h3>
+                        <p>
+                            Proyectos con Certificacion LEED por
+                            eficiencia energética, iluminación natural
+                            y ventilación diseñada.
+                        </p>
+                    </div>
+                    <div className={styles.navigation}>
+                        <h3>Navegación</h3>
+                        <ul>{list.map(item =>
+                            <li key={item}><Link to='/'>{item}</Link></li>
+                        )}
+                        </ul>
+                    </div>
+                    <div className={styles.contacts}>
+                        <h3>Contacto</h3>
+                        <ul>
+                            <li><Link to='/'>Servicio postventa</Link></li>
+                            <li><Link to='/'>Correo electrónico</Link></li>
+                        </ul>
+                    </div>
+                    <div className={styles.social}>
+                        <h3>Redes Sociales</h3>
+                        <ul>
+                            <li><Link to='/'>Siguenos en</Link></li>
+                        </ul>
+                        <Link to='/'><img src={facebookIcon} alt='facebook' /></Link>
+                        <Link to='/'><img src={instagramIcon} alt='instagram' /></Link>
+                    </div>
                 </div>
-                <div className={styles.navigation}>
-                    <h3>Navegación</h3>
-                    <ul>
-                        <li><Link to='/'>Nosotros</Link></li>
-                        <li><Link to='/'>Vertical</Link></li>
-                        <li><Link to='/'>Horizontal</Link></li>
-                        <li><Link to='/'>Comercial</Link></li>
-                        <li><Link to='/'>Oficinas</Link></li>
-                        <li><Link to='/'>Financiamiento</Link></li>
-                        <li><Link to='/'>Relación con Inversionistas</Link></li>
-                    </ul>
-                </div>
-                <div className={styles.contacts}>
-                    <h3>Contacto</h3>
-                    <ul>
-                        <li><Link to='/'>Servicio postventa</Link></li>
-                        <li><Link to='/'>Correo electrónico</Link></li>
-                    </ul>
-                </div>
-                <div className={styles.social}>
-                    <h3>Redes Sociales</h3>
-                    <ul>
-                        <li><Link to='/'>Siguenos en</Link></li>
-                    </ul>
-                    <Link to='/'><img src={facebookIcon} alt='facebook' /></Link>
-                    <Link to='/'><img src={instagramIcon} alt='instagram' /></Link>
-                </div>
-            </section>
-            <section className={styles.partners}>
+                <div className={styles.partners}>
 
-            </section>
+                </div>
+            </div>
         </footer>
     );
 };
