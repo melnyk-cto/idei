37b5cc03afdb9e6fc95ca649c04543d560f4323d
Mykola <melnyk.cto.numo@gmail.com>
2020-04-07 05:28:19 +0300

    Updates

---
+++ precache-manifest.8a971f2f50a990bbfcfe3c02d6370c73.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85ff88c3f1a7d575f73a3983ec2f048",
+    "url": "/idei/index.html"
+  },
+  {
+    "revision": "fb6d329d32347742a1a7",
+    "url": "/idei/static/css/main.1348bc2b.chunk.css"
+  },
+  {
+    "revision": "d23c09d9a596a4bb7864",
+    "url": "/idei/static/js/2.88bab9df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/idei/static/js/2.88bab9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6d329d32347742a1a7",
+    "url": "/idei/static/js/main.c55564d4.chunk.js"
+  },
+  {
+    "revision": "1e326c465e1d048a03a6",
+    "url": "/idei/static/js/runtime-main.86ff6c07.js"
+  },
+  {
+    "revision": "ea15d90c8a3eb2b6f16491031b01ab81",
+    "url": "/idei/static/media/banner.ea15d90c.png"
+  },
+  {
+    "revision": "a0277afb8605250ed47ca8018e4398ca",
+    "url": "/idei/static/media/espacia-logo.a0277afb.svg"
+  },
+  {
+    "revision": "e8ad2af3fdb3d9842c6e3c70f2e73418",
+    "url": "/idei/static/media/facebook-icon.e8ad2af3.svg"
+  },
+  {
+    "revision": "eac8dc49b0a5d3d922f599e0d70a399e",
+    "url": "/idei/static/media/instagram-icon.eac8dc49.svg"
+  },
+  {
+    "revision": "6397aa15e43c93b1a6bdc22f9e398e0a",
+    "url": "/idei/static/media/leed-logo.6397aa15.svg"
+  },
+  {
+    "revision": "e3cff33a1f5ac2a5dcb328b1e0346c73",
+    "url": "/idei/static/media/login.e3cff33a.svg"
+  },
+  {
+    "revision": "bc21f080320a6475b4498a507eca57ac",
+    "url": "/idei/static/media/logo.bc21f080.svg"
+  },
+  {
+    "revision": "d9851adc70233c30c1b6faa4471a023c",
+    "url": "/idei/static/media/reeliza-logo.d9851adc.svg"
+  },
+  {
+    "revision": "d51c7d299158de3c81a0c56320188705",
+    "url": "/idei/static/media/trazzo-logo.d51c7d29.svg"
+  }
+]);
