3182c89d022c98a79138ec88e0216694df383952
Mykola <melnyk.cto.numo@gmail.com>
2020-04-07 04:21:11 +0300

    features: create header

--- src/componets/common/Header/Header.jsx
+++ src/componets/common/Header/Header.jsx
@@ -1,13 +1,52 @@
 // core
-import React from 'react';
+import React, { useState } from 'react';
+
+// library
+import { Link } from 'react-router-dom';
+
+// images
+import banner from '../../../assets/images/banner.png'
+import logo from '../../../assets/images/logo.svg'
+import login from '../../../assets/images/login.svg'
+import trazzo from '../../../assets/images/trazzo-logo.svg'
 
 // styles
 import styles from './Header.module.scss';
 
+const menus = ['VERTICAL', 'HORIZONTAL', 'COMERCIAL', 'FINANCIAMIENTO', 'RELACION', 'RELACION CON INVERSIONISTAS '];
 export const Header = () => {
+
+    const [active, setActive] = useState(false);
+    const addClass = () => {
+        setActive(!active);
+    };
+
     return (
         <header className={styles.header}>
-            Header
+            <img src={banner} alt='banner' className={styles.banner} />
+            <div className={styles.menuWrapper}>
+                <div className={styles.logo}>
+                    <img src={logo} alt='logo' />
+                </div>
+                <menu className={active ? [styles.menu + ' ' + styles.active] : styles.menu}>
+                    <ul>
+                        {menus.map(item => <li key={item}>
+                            <Link to='/'>
+                                {item}
+                            </Link>
+                            <Link to='/' className={styles.hover}>
+                                {item}
+                                <img src={trazzo} alt='' />
+                            </Link>
+                        </li>)}
+                    </ul>
+                </menu>
+                <button type='button' className={active ? [styles.burgerMenu + ' ' + styles.active] : styles.burgerMenu}
+                        onClick={() => addClass()}>
+                    <span className={styles.burgerMenuLines} />
+                </button>
+                <img src={login} alt='login' className={styles.login} />
+            </div>
         </header>
     );
 };
--- src/componets/pages/Home/Home.jsx
+++ src/componets/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import styles from './Home.module.scss';
 export const Home = () => {
     return (
         <section className={styles.home}>
-            Trazzo
+            Home
         </section>
     );
 };
